5a43661dff0ea6c27aaedea65ebff9cda5c49d00
Kaksi3118 <maksymilian.jura3118@gmail.com>
2024-11-25 01:25:16 +0100

    feat: added test_managing_list to test operations on students

---
+++ test_managing_list.py
@@ -0,0 +1,36 @@
+import pytest
+
+from managingList import managingListClass
+
+def test_add_student():
+    # Given:
+    manager = managingListClass()
+    want = {
+        "name:": "Anna",
+        "surname:": "Nowak",
+        "id:": "2",
+        "date:": "2023-11-25"
+    }
+
+    # When:
+    manager.addStudent("Anna", "Nowak", "2", "2023-11-25")
+    got = manager.students[0]
+
+    # Then:
+    assert got == want
+
+def test_delete_student(tmp_path):
+    # Given:
+    test_file = tmp_path / "students.csv"
+    manager = managingListClass()
+    manager.addStudent("Anna", "Nowak", "2", "2023-11-25")
+    manager.saveToFile(test_file)
+    want = 1  # Liczba wierszy w pliku po usunięciu (nagłówek)
+
+    # When:
+    manager.deleteStudent("2", test_file)
+    with open(test_file, "r", encoding="utf-8") as file:
+        got = len(file.readlines())
+
+    # Then:
+    assert got == want
