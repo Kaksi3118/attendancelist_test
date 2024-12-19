2fcb82508fe28ca73d721ecf38d0b3f0d3cc29c9
Maksymilian Jura <maksymilian.jura3118@gmail.com>
2024-12-19 10:04:42 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+version = os.getenv(
+    "VERSION", "0.0.0"
+)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
+
+
+
+setup(
+    name="attendancelist_test",
+    version=version,
+    author="Maksymilian Jura",
+    author_email="maksymilian.jura3118@gmail.com",
+    description="Attendance list for uni",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kaksi3118/attendancelist_test",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
